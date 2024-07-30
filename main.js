88f3e9941e16656eecb38614ce72a3155cb29a38
EnzoYK <128195019+EnzoYK@users.noreply.github.com>
2024-07-30 08:52:38 -0300

    ..

--- main.js
+++ main.js
@@ -1,9 +1,3 @@
-const botoes = document.querySelectorAll('.parametro-senha_botao');
-botoes[0].onclick = diminuiTamanho;
-botoes[1].onclick = aumentaTamanho;
-
-console.log(botoes);
-
 const numeroSenha = document.querySelector('.parametro-senha_texto');
 let tamanhoSenha = 12;
 numeroSenha.textContent = tamanhoSenha;
